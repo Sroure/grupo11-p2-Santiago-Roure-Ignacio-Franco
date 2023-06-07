8ef3cd9a439cf0a8e227af68e76b63849d6606df
Sroure <131919060+Sroure@users.noreply.github.com>
2023-06-06 21:08:47 -0300

    Delete TADS_Santi/ejercicio1 directory

--- TADS_Santi/ejercicio1/src/main/java/Hash/MyHash.java
+++
@@ -1,7 +0,0 @@
-package Hash;
-
-public interface MyHash<K, V> {
-    void insert(K key, V value);
-    void delete(K key);
-    boolean search(K key);
-}
--- TADS_Santi/ejercicio1/src/main/java/Queue/MyQueue.java
+++
@@ -1,8 +0,0 @@
-package Queue;
-
-public interface MyQueue <T> {
-    void enqueue (T element);
-    T dequeue () throws EmptyQueueException;
-    boolean esVacio();
-    void imprimirDatos();
-}
