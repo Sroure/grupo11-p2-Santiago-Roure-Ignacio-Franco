a8e8e2a26bc3fb7d215f09c1ff284eb16dd19e20
Sroure <sroure@correo.um.edu.uy>
2023-06-23 18:29:42 -0300

    trabajo del primer metodo

--- grupo11-p2-Obligatorio/Obligatorio/Main/Consultas.java
+++ grupo11-p2-Obligatorio/Obligatorio/Main/Consultas.java
@@ -17,16 +17,22 @@ import static Main.MainMenu.CSVReader.ListaTweets;
 public class Consultas {
     // (1) Primera consulta
     public static void ListarPilotosMasMencionados(String mes, MyHashImpl<Long, Tweet> ListaTweets, Lista<Piloto> ListaPilotos) {
-        MyHeap<Piloto> mencionesPilotos = new MyHeapImpl<>(100);// quiero recorrer la lista de tweets y fijarme la cantidad de veces que aparece cada piloto en los tweets
+        MyHeap<Piloto> mencionesPilotos = new MyHeapImpl<>(20);// quiero recorrer la lista de tweets y fijarme la cantidad de veces que aparece cada piloto en los tweets
         for (int i = 0; i < ListaTweets.elementosEnTabla(); i++) {// elementos en la talbla es el size
-            for (int j = 0; j < ListaPilotos.largo(); j++) {
-                if (ListaTweets.get((long) i).getFechaTweet().contains(mes)) {// verifico si el tweet es de ese mes
-                    if (ListaTweets.get((long) i).getContent().contains(ListaPilotos.get(j).getNombre())) {
-                        ListaPilotos.get(j).setCantidadMencion(ListaPilotos.get(j).getCantidadMencion() + 1);
-                        for (int h = 0; h < ListaPilotos.largo(); h++) {
-
+            if (ListaTweets.get((long) i).getFechaTweet().contains(mes)){
+                for (int j = 0; j < ListaPilotos.largo(); j++) {
+                    // verifico si el tweet es de ese piloto
+                        if (ListaTweets.get((long) i).getContent().contains(ListaPilotos.get(j).getNombre())) {
+                            ListaPilotos.get(j).setCantidadMencion(ListaPilotos.get(j).getCantidadMencion() + 1);
+                            mencionesPilotos.insert(ListaPilotos.get(j));
+                            break;
+                            // ahora lo que quiero es insertarlos en la lista menciones pilotos;
                         }
-                    }
+                        for (int x = 0; x < 10; x++) {
+                            System.out.println(mencionesPilotos.getMax().getNombre());
+                            mencionesPilotos.delete();
+                        }
+
                 }
             }
         }
